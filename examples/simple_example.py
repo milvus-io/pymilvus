96f0aefbc039bf97bfa9a40c3c8bb943370a9b1a
Jin Hai <hai.jin@zilliz.com>
2019-06-21 15:38:29 +0800

    Update simple_example.py

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -1,19 +1,21 @@
+# This program demos how to create a Milvus table, insert 20 vectors and get the table row count.
+
 from milvus import Milvus, Prepare, IndexType, Status
 import random
 
 milvus = Milvus()
 
-# Connect Milvus server, please change HOST and PORT to correct one
+# Connect Milvus server.
+# You may need to change HOST and PORT accordingly.
 milvus.connect(host='localhost', port='33001')
 
-
 # Table name is defined
-table_name = 'table_'+str(random.randint(0,100))
+table_name = 'demo_table'
 
 # Create table: table name, vector dimension and index type
 milvus.create_table(Prepare.table_schema(table_name, dimension=256, index_type=IndexType.IDMAP))
 
-# Add 20 256-dim-vectors into table
+# Insert 20 256-dim-vectors into demo_table
 vectors = Prepare.records([[random.random()for _ in range(256)] for _ in range(20)])
 milvus.add_vectors(table_name=table_name, records=vectors)
 
