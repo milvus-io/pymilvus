4aba791802dc7a65705ac6ba05349946349233c7
Yang Xuan <jumpthepig@gmail.com>
2019-06-21 20:29:38 +0800

    Prepare version-0.1.5

--- milvus/client/Client.py
+++ milvus/client/Client.py
@@ -34,7 +34,7 @@ else:
 
 LOGGER = logging.getLogger(__name__)
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __NAME__ = 'pymilvus'
 
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / 'README.md').read_text()
 
 setuptools.setup(
     name="pymilvus",
-    version="0.1.4",
+    version="0.1.5",
     description="Python Sdk for Milvus",
     long_description=README,
     long_description_content_type='text/markdown',
