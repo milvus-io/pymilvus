f18ca3d099d7692a01c2868216b4007eaab31453
yinghao.zou <yinghao.zou@zilliz.com>
2019-08-14 19:50:52 +0800

    fix gen_vectors.py error

--- tests/gen_vectors.py
+++ tests/gen_vectors.py
@@ -25,54 +25,49 @@ def grpc_time_without_prepare():
         'store_raw_vector': False
     })
 
+    vectors = gen_vectors(num=NUM, dim=DIM)
 
-vectors = gen_vectors(num=NUM, dim=DIM)
+    before = time.perf_counter()
+    insertinfo = gPrepare.insert_param(table_name, vectors)
+    delt = time.perf_counter() - before
 
-before = time.perf_counter()
+    grpc_add_vectors_without_prepare(mi, insertinfo)
 
-insertinfo = gPrepare.insert_param(table_name, vectors)
+    time.sleep(5)
 
-delt = time.perf_counter() - before
-
-grpc_add_vectors_without_prepare(mi, insertinfo)
-
-time.sleep(5)
-
-_, n = mi.get_table_row_count(table_name)
-print(f"[grpc] add {NUM} vectors successfully, total: {n}")
-print(f"gRPC Serializing costs: {delt}")
+    _, n = mi.get_table_row_count(table_name)
+    print(f"[grpc] add {NUM} vectors successfully, total: {n}")
+    print(f"gRPC Serializing costs: {delt}")
 
 
 def grpc_time():
     mi = Milvus()
     mi.connect()
 
+    if mi.has_table(table_name):
+        mi.delete_table(table_name)
+        time.sleep(2)
 
-if mi.has_table(table_name):
-    mi.delete_table(table_name)
-    time.sleep(2)
+    mi.create_table({
+        'table_name': table_name,
+        'dimension': DIM,
+        'index_type': IndexType.FLAT,
+        'store_raw_vector': False
+    })
 
-mi.create_table({
-    'table_name': table_name,
-    'dimension': DIM,
-    'index_type': IndexType.FLAT,
-    'store_raw_vector': False
-})
+    vectors = gen_vectors(num=NUM, dim=DIM)
 
-vectors = gen_vectors(num=NUM, dim=DIM)
+    gen(mi, table_name, vectors)
 
-gen(mi, table_name, vectors)
+    time.sleep(5)
 
-time.sleep(5)
+    _, n = mi.get_table_row_count(table_name)
+    print(f"[grpc] add {NUM} vectors successfully, total: {n}")
 
-_, n = mi.get_table_row_count(table_name)
-print(f"[grpc] add {NUM} vectors successfully, total: {n}") \
 
 @time_it
 def grpc_add_vectors_without_prepare(milvus, insertinfo):
-
-
-ids = milvus.add_vectors(None, None, insert_param=insertinfo)
+    ids = milvus.add_vectors(None, None, insert_param=insertinfo)
 
 
 @time_it
