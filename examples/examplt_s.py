c2c017480424369731f57497a5a674ff7c53c94a
yhz <413554850@qq.com>
2019-11-02 18:12:27 +0800

    add example

--- examples/examplt_s.py
+++ examples/examplt_s.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         # records = [[random.random() for j in range(128)] for i in range(1000000)]
         client.add_vectors(table_name, records=records, timeout=3000)
 
-    query_records = [np.random.rand(128).astype(np.float32).tolist() for i in range(10000)]
+    query_records = [np.random.rand(128).astype(np.float32).tolist() for i in range(1000)]
     print("[{}] <Interface> Start search ....".format(datetime.datetime.now()))
     client.search_vectors(table_name, top_k=1000, nprobe=16, query_records=query_records)
     print("[{}] <Interface> Search done.".format(datetime.datetime.now()))
