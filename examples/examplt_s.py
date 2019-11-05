f37ad78d57ea47f89f5948263298e17fa62fe2b9
yhz <413554850@qq.com>
2019-11-05 12:53:12 +0800

    update s

--- examples/examplt_s.py
+++ examples/examplt_s.py
@@ -5,11 +5,30 @@ import datetime
 sys.path.append(".")
 from milvus import Milvus, IndexType
 
+sift_1b_base_file = "/exp/hdd1/data/faiss_assets/ANN_SIFT1B/dataset/bigann_base.bvecs"
+sift_1b_query_file = "/exp/hdd1/data/faiss_assets/ANN_SIFT1B/dataset/bigann_query.bvecs"
+_NB = 1000 * 1000
+_NQ = 10000
+_Top_K = 1000
+
+
+def mmap_fvecs(fname):
+    x = np.memmap(fname, dtype='int32', mode='r')
+    d = x[0]
+    return x.view('float32').reshape(-1, d + 1)[:, 1:]
+
+
+def mmap_bvecs(fname):
+    x = np.memmap(fname, dtype='uint8', mode='r')
+    d = x[:4].view('int32')[0]
+    return x.reshape(-1, d + 4)[:, 4:]
+
+
 if __name__ == '__main__':
     client = Milvus()
-    client.connect(host="localhost", port="19531")
+    client.connect(host="192.168.1.113", port="19531")
 
-    table_name = "table_name_test"
+    table_name = "table_test"
 
     status, ok = client.has_table(table_name)
     if not ok:
@@ -21,11 +40,19 @@ if __name__ == '__main__':
         )
 
         print("Create table Done, Start add vectors")
-        records = [np.random.rand(128).astype(np.float32).tolist() for i in range(1000000)]
+        # records = [np.random.rand(128).astype(np.float32).tolist() for i in range(1000000)]
+        records = sift_xb = mmap_bvecs(sift_1b_base_file)[:_NB].astype(np.float32).tolist()
         # records = [[random.random() for j in range(128)] for i in range(1000000)]
         client.add_vectors(table_name, records=records, timeout=3000)
 
-    query_records = [np.random.rand(128).astype(np.float32).tolist() for i in range(1000)]
+        print("Start create index")
+        status = client.create_index(table_name, {"index_type": IndexType.IVFLAT, "nlist": 4096})
+        if status.OK():
+            print("Create index OK")
+
+    # query_records = [np.random.rand(128).astype(np.float32).tolist() for i in range(_NQ)]
+    query_records = mmap_bvecs(sift_1b_query_file)[:_NQ].astype(np.float32).tolist()
+
     print("[{}] <Interface> Start search ....".format(datetime.datetime.now()))
-    client.search_vectors(table_name, top_k=1000, nprobe=16, query_records=query_records)
+    client.search_vectors(table_name, top_k=_Top_K, nprobe=16, query_records=query_records)
     print("[{}] <Interface> Search done.".format(datetime.datetime.now()))
