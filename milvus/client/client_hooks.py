9b662d9bedf8f815bac0a35e873905d35f9574ed
yhz <413554850@qq.com>
2019-11-09 09:40:35 +0800

    remove print info in search hook

--- milvus/client/client_hooks.py
+++ milvus/client/client_hooks.py
@@ -10,15 +10,9 @@ class SearchHook(BaseaSearchHook):
 
     def pre_search(self, *args, **kwargs):
         super().pre_search(*args, **kwargs)
-        self._time_stamp0 = datetime.datetime.now()
-        print("[{}] <Intraval> Start search ....".format(self._time_stamp0))
 
     def aft_search(self, *args, **kwargs):
         super().aft_search(*args, **kwargs)
-        time_stamp0 = datetime.datetime.now()
-        print("[{}] <Interval> Search done ....".format(time_stamp0))
-        time_r = time_stamp0 - self._time_stamp0
-        print("Search interface cost {} ms".format(time_r.seconds * 1000 + time_r.microseconds // 1000))
 
     def on_response(self, *args, **kwargs):
         return super().on_response(*args, **kwargs)
