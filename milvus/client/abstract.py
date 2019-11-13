30047dd9c4ee1df494c86bd9c3a7218bf39f0282
yhz <413554850@qq.com>
2019-11-13 19:56:07 +0800

    modify docstring in class TableSchame

--- milvus/client/abstract.py
+++ milvus/client/abstract.py
@@ -10,28 +10,27 @@ from .types import IndexType
 
 
 class TableSchema:
-    """
-    Table Schema
-
-    :type  table_name: str
-    :param table_name: (Required) name of table
-
-        `IndexType`: 0-invalid, 1-flat, 2-ivflat, 3-IVF_SQ8, 4-MIX_NSG
-
-    :type  dimension: int64
-    :param dimension: (Required) dimension of vector
-
-    :type  index_file_size: int64
-    :param index_file_size: (Optional) max size of files which store index
-
-    :type  metric_type: MetricType
-    :param metric_type: (Optional) vectors metric type
-
-        `MetricType`: 1-L2, 2-IP
-
-    """
-
     def __init__(self, table_name, dimension, index_file_size, metric_type):
+        """
+        Table Schema
+
+        :type  table_name: str
+        :param table_name: (Required) name of table
+
+            `IndexType`: 0-invalid, 1-flat, 2-ivflat, 3-IVF_SQ8, 4-MIX_NSG
+
+        :type  dimension: int64
+        :param dimension: (Required) dimension of vector
+
+        :type  index_file_size: int64
+        :param index_file_size: (Optional) max size of files which store index
+
+        :type  metric_type: MetricType
+        :param metric_type: (Optional) vectors metric type
+
+            `MetricType`: 1-L2, 2-IP
+
+        """
         check_pass_param(table_name=table_name, dimension=dimension,
                          index_file_size=index_file_size, metric_type=metric_type)
 
