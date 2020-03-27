a9e667c0e9c41f57323d786da807e206829486d2
Yhz <yinghao.zou@zilliz.com>
2020-03-27 15:30:40 +0800

    update case  fix #170, #171, #172, #174, #175, #177
    Signed-off-by: Yhz <yinghao.zou@zilliz.com>

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,3 +1,14 @@
+import random
+
+import pytest
+
+from milvus import ParamError
+from factorys import records_factory
+
+dim = 128
+nq = 100
+
+
 class TestFlush:
     def test_flush(self, gcon):
         collection_param = {
@@ -40,3 +51,65 @@ class TestFlush:
 
         for collection in collection_list:
             gcon.drop_collection(collection)
+
+
+class TestCompact:
+    def test_compact_normal(self, gcon, gcollection):
+        vectors = [[random.random() for _ in range(128)] for _ in range(10000)]
+        status, ids = gcon.add_vectors(collection_name=gcollection, records=vectors)
+        assert status.OK()
+
+        status = gcon.compact(gcollection)
+        assert status.OK(), status.message
+
+    def test_compact_after_delete(self, gcon, gcollection):
+        vectors = [[random.random() for _ in range(128)] for _ in range(10000)]
+        status, ids = gcon.insert(collection_name=gcollection, records=vectors)
+        assert status.OK(), status.message
+
+        status = gcon.flush([gcollection])
+        assert status.OK(), status.message
+
+        status = gcon.delete_by_id(gcollection, ids[100:1000])
+        assert status, status.message
+
+        status = gcon.compact(gcollection)
+        assert status.OK(), status.message
+
+    def test_compact_with_empty_collection(self, gcon, gcollection):
+        status = gcon.compact(gcollection)
+        assert status.OK(), status.message
+
+    def test_compact_with_non_exist_name(self, gcon):
+        status = gcon.compact(collection_name="die333")
+        assert not status.OK()
+
+    @pytest.mark.parametrize("name", [123, None, [123], True, {}])
+    def test_compact_with_invalid_name(self, name, gcon):
+        with pytest.raises(ParamError):
+            gcon.compact(collection_name=name)
+
+
+class TestCmd:
+    def test_cmd_version(self, gcon):
+        status, reply = gcon.server_version()
+        assert status.OK()
+
+    @pytest.mark.parametrize("cmd", ["version", "status", "mode", "tasktable", "get_system_info", "build_commit_id"])
+    def test_cmd_whole(self, cmd, gcon):
+        status, reply = gcon._cmd(cmd)
+        assert status.OK()
+
+
+class TestConfig:
+    def test_get_config(self, gcon):
+        status, value = gcon.get_config("cache_config", "cpu_cache_capacity")
+        assert status.OK()
+
+    def test_set_config(self, gcon):
+        status, value = gcon.get_config("cache_config", "cpu_cache_capacity")
+        assert status.OK()
+
+        status, reply = gcon.set_config("cache_config", "cpu_cache_capacity", value)
+        assert status.OK()
+
