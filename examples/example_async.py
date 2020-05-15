5df827bb11395bfbfcae01cd03b19a52039586f8
yhz <413554850@qq.com>
2020-05-15 13:05:58 +0800

    update example
    Signed-off-by: yhz <413554850@qq.com>

--- examples/example_async.py
+++ examples/example_async.py
@@ -27,7 +27,7 @@ def main():
     milvus = Milvus(_HOST, _PORT)
 
     # Create collection demo_collection if it dosen't exist.
-    collection_name = 'example_collection_'
+    collection_name = 'example_async_collection_'
 
     status, ok = milvus.has_collection(collection_name)
     if not ok:
@@ -38,19 +38,24 @@ def main():
             'metric_type': MetricType.L2  # optional
         }
 
-        milvus.create_collection(param)
+        status = milvus.create_collection(param)
+        if not status.OK():
+            print("Create collection failed: {}".format(status.message), file=sys.stderr)
+            print("exiting ...", file=sys.stderr)
+            sys.exit(1)
 
     # Show collections in Milvus server
-    _, collections = milvus.show_collections()
+    _, collections = milvus.list_collections()
 
     # Describe demo_collection
-    _, collection = milvus.describe_collection(collection_name)
+    _, collection = milvus.get_collection_info(collection_name)
     print(collection)
 
     # 10000 vectors with 16 dimension
     # element per dimension is float32 type
     # vectors should be a 2-D array
     vectors = [[random.random() for _ in range(_DIM)] for _ in range(100000)]
+
     # You can also use numpy to generate random vectors:
     #     `vectors = np.random.rand(10000, 16).astype(np.float32)`
 
@@ -61,10 +66,8 @@ def main():
             print("Insert failed.", status.message)
 
     # Insert vectors into demo_collection, return status and vectors id list
-    insert_future = milvus.insert(collection_name=collection_name, records=vectors, _async=True, _callback=_insert_callback)
-    time.sleep(2)
-    status, reply = milvus._cmd("requests") #, timeout=1000)
-    print("Reply: {}".format(reply))
+    insert_future = milvus.insert(collection_name=collection_name, records=vectors, _async=True,
+                                  _callback=_insert_callback)
     insert_future.done()
 
     # Flush collection  inserted data to disk.
@@ -73,6 +76,7 @@ def main():
             print("Flush successfully")
         else:
             print("Flush failed.", status.message)
+
     flush_future = milvus.flush([collection_name], _async=True, _callback=_flush_callback)
     flush_future.done()
 
@@ -81,14 +85,15 @@ def main():
             print("Compact successfully")
         else:
             print("Compact failed.", status.message)
+
     compact_furure = milvus.compact(collection_name, _async=True, _cakkback=_compact_callback)
     compact_furure.done()
 
     # Get demo_collection row count
-    status, result = milvus.count_collection(collection_name)
+    status, result = milvus.count_entities(collection_name)
 
     # present collection info
-    _, info = milvus.collection_info(collection_name)
+    _, info = milvus.get_collection_stats(collection_name)
     print(info)
 
     # create index of vectors, search more rapidly
@@ -101,15 +106,17 @@ def main():
             print("Create index successfully")
         else:
             print("Create index failed.", status.message)
+
     # Create ivflat index in demo_collection
     # You can search vectors without creating index. however, Creating index help to
     # search faster
     print("Creating index: {}".format(index_param))
-    index_future = milvus.create_index(collection_name, IndexType.IVF_FLAT, index_param, _async=True, _callback=_index_callback)
+    index_future = milvus.create_index(collection_name, IndexType.IVF_FLAT, index_param, _async=True,
+                                       _callback=_index_callback)
     index_future.done()
 
     # describe index, get information of index
-    status, index = milvus.describe_index(collection_name)
+    status, index = milvus.get_index_info(collection_name)
     print(index)
 
     # Use the top 10 vectors for similarity search
@@ -131,7 +138,7 @@ def main():
             # indicate search result
             # also use by:
             #   `results.distance_array[0][0] == 0.0 or results.id_array[0][0] == ids[0]`
-            if results[0][0].distance == 0.0: # or results[0][0].id == ids[0]:
+            if results[0][0].distance == 0.0:  # or results[0][0].id == ids[0]:
                 print('Query result is correct')
             else:
                 print('Query result isn\'t correct')
--- examples/example_vectors.py
+++ examples/example_vectors.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     client.flush([collection_name])
 
     # query collection's statistical information.
-    status, info = client.collection_info(collection_name)
+    status, info = client.get_collection_stats(collection_name)
     if not status.OK():
         print("Query collection statistical information fail. exiting ....")
         sys.exit(1)
@@ -35,28 +35,28 @@ if __name__ == '__main__':
     # show collection information
     print("Total amount of vectors in collection {} is {}".format(collection_name, info["row_count"]))
     for par in info["partitions"]:
-        print("\tpartition tag: {}, vector count: {}".format(par.tag, par.count))
+        print("\tpartition tag: {}, vector count: {}".format(par["tag"], par["row_count"]))
         # show segment information
-        for seg in par.segments_stat:
+        for seg in par["segments"]:
             print("\t\tsegment name: {}, vector count: {}, index: {}, storage size {:.3f} MB"
-                  .format(seg.segment_name, seg.count, seg.index_name, seg.data_size / 1024 / 1024))
+                  .format(seg["name"], seg["row_count"], seg["index_name"], seg["data_size"] / 1024 / 1024))
 
     # obtain vector ids from segment, then
     # get vector by specifying vector id
-    segment0 = info.partitions_stat[0].segments_stat[0]
-    status, ids = client.get_vector_ids(collection_name, segment0.segment_name)
+    segment0 = info["partitions"][0]["segments"][0]
+    status, ids = client.list_id_in_segment(collection_name, segment0["name"])
     if not status.OK():
-        print("Cannot obtain vector ids from segment {}. exiting ....".format(segment0.segment_name))
+        print("Cannot obtain vector ids from segment {}. exiting ....".format(segment0["name"]))
         sys.exit(1)
 
-    # obtain first vector
-    status, vector = client.get_vector_by_id(collection_name, ids[0])
+    # obtain top 5 vector
+    status, vectors = client.get_entity_by_id(collection_name, ids[:5])
     if not status.OK():
         print("Cannot obtain vector. exiting ....")
         sys.exit(1)
 
     # delete top 10 vectors
-    status = client.delete_by_id(collection_name, ids[:10])
+    status = client.delete_entity_by_id(collection_name, ids[:10])
     if status.OK():
         print("Delete top 10 vectors successfully")
     else:
