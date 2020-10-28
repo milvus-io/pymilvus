8e71f83bb11382d8752d381419ff1aaefc479c26
yinghao.zou <yinghao.zou@zilliz.com>
2020-10-28 14:52:07 +0800

    update requirements
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>

---
+++ doc/source/about.rst
@@ -0,0 +1,14 @@
+=========================
+About This Documentation
+=========================
+
+
+This documentation is generated using the Sphinx documentation generator. The source files for the documentation are
+located in the *doc/* directory of the pymilvus distribution. To generate the docs locally run the following command
+under directory *doc/*:
+
+>>> make html
+
+
+
+
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'm2r',
+    'm2r2',
 ]
 
 # Add any source suffix
@@ -60,8 +60,8 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
+# html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
