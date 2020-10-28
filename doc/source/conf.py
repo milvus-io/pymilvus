38f7369610a61c3a8f06be810c3f4ce43f3298b3
XuanYang-cn <51370125+XuanYang-cn@users.noreply.github.com>
2020-10-28 16:11:44 +0800

    add overall (#324)
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>
    
    Co-authored-by: yangxuan <xuan.yang@zilliz.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -62,6 +62,8 @@ exclude_patterns = []
 # a list of builtin themes.
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+html_show_sphinx = False
+copyright = '2019-2020 Zilliz. All rights reserved.'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
