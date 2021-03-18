fe7ce748bc6e218d62224867bceb5c5255b75294
BossZou <yinghao.zou@zilliz.com>
2021-03-18 17:40:54 +0800

    Add CI and Mergify (#459)
    * Remove many unsed code
    
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>
    
    * Update third-party package require
    
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>
    
    * Remove numpy code
    
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>
    
    * try test
    
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>
    
    * Add git ci code
    
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>
    
    * update gitignore
    
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>
    
    * Add mergify
    
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>
    
    * Update mergify logic
    
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>
    
    * [ci] Update format
    
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     license="Apache-2.0",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=["grpcio>=1.22.0", "grpcio-tools>=1.22.0", "requests>=2.22.0", "ujson>=1.35", "numpy>=1.16.3"],
+    install_requires=["grpcio>=1.22.0", "grpcio-tools>=1.22.0", "requests>=2.22.0", "ujson>=2.0.0"],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
