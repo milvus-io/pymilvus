c0ddbc29a31e7019deaed288cd4d5a8aea408077
Xiangyu Wang <xiangyu.wang@zilliz.com>
2021-04-19 16:50:10 +0000

    Update interfaces in utility module
    * Update interfaces in utility module
    
    Signed-off-by: Xiangyu Wang <wxyucs@gmail.com>

--- orm/pymilvus_orm/utility.py
+++ orm/pymilvus_orm/utility.py
@@ -1,23 +1,20 @@
-
-
-
-def loading_progress(collection_name, partition_name=None):
+def loading_progress(collection_name, partition_name=""):
     """
     Show #loaded entities vs #total entities.
 
-    :param collection_name: The name of collection to show.
+    :param collection_name: The name of collection to show
     :type  collection_name: str
 
-    :param partition_name: The name of partition to show.
+    :param partition_name: The name of partition to show
     :type  partition_name: str
 
-    :return: Number of loaded entities and total entites
-    :rtype:  (int, int)
+    :return: Loading progress, contains num of loaded and num of total
+    :rtype:  dict
     """
     pass
 
 
-def wait_for_loading_complete(collection_name, partition_name=None):
+def wait_for_loading_complete(collection_name, partition_name="", timeout=None):
     """
     Block until loading is done or Raise Exception after timeout.
 
@@ -26,35 +23,44 @@ def wait_for_loading_complete(collection_name, partition_name=None):
 
     :param partition_name: The name of partition to wait
     :type  partition_name: str
+
+    :param timeout: The timeout for this method, unit: second
+    :type  timeout: int
     """
     pass
 
 
-def index_building_progress(collection_name, index_name=None):
+def index_building_progress(collection_name, index_name, timeout=None):
     """
     Show # indexed entities vs. # total entities.
 
-    :param collection_name: The name of collection to show.
+    :param collection_name: The name of collection to show
     :type  collection_name: str
 
-    :param index_name: The name of index to show.
+    :param index_name: The name of index to show
     :type  index_name: str
 
-    :return: Number of indexed entities and total entites
-    :rtype:  (int, int)
+    :param timeout: The timeout for this method, unit: second
+    :type  timeout: int
+
+    :return: Building progress, contains num of indexed entities and num of total entities
+    :rtype:  dict
     """
     pass
 
 
-def wait_for_index_building_complete(collection_name, index_name=None):
+def wait_for_index_building_complete(collection_name, index_name, timeout=None):
     """
     Block until building is done or Raise Exception after timeout.
 
     :param collection_name: The name of collection to wait
     :type  collection_name: str
 
-    :param index_name: The name of index to wait.
+    :param index_name: The name of index to wait
     :type  index_name: str
+
+    :param timeout: The timeout for this method, unit: second
+    :type  timeout: int
     """
     pass
 
@@ -72,12 +78,12 @@ def has_collection(collection_name):
     pass
 
 
-def has_partition(collecton_name, partition_name):
+def has_partition(collection_name, partition_name):
     """
     Checks if a specified partition exists in a collection.
 
-    :param collecton_name: The collection name of partition to check
-    :type  collecton_name: str
+    :param collection_name: The collection name of partition to check
+    :type  collection_name: str
 
     :param partition_name: The name of partition to check.
     :type  partition_name: str
---
+++ orm/tests/test_utility.py
@@ -0,0 +1,39 @@
+import unittest
+import random
+from pymilvus_orm.utility import *
+
+
+def gen_collection_name():
+    return f'ut-collection-' + str(random.randint(100000, 999999))
+
+
+def gen_partition_name():
+    return f'ut-partition-' + str(random.randint(100000, 999999))
+
+
+def gen_index_name():
+    return f'ut-index-' + str(random.randint(100000, 999999))
+
+
+class TestCase(unittest.TestCase):
+    def test_loading_progress(self):
+        loading_progress(gen_collection_name(), gen_partition_name())
+
+    def test_wait_for_loading_complete(self):
+        wait_for_loading_complete(gen_collection_name(), gen_partition_name())
+
+    def test_index_building_progress(self):
+        index_building_progress(gen_collection_name(), gen_index_name())
+
+    def test_wait_for_index_building_complete(self):
+        wait_for_index_building_complete(gen_collection_name(), gen_index_name())
+
+    def test_has_collection(self):
+        has_collection(gen_collection_name())
+
+    def test_has_partition(self):
+        has_partition(gen_collection_name(), gen_partition_name())
+
+
+if __name__ == '__main__':
+    unittest.main()
