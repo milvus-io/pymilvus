33a2fb5bb1fbb7473488303350b6532eb0e066d5
Xiangyu Wang <xiangyu.wang@zilliz.com>
2021-05-21 10:48:14 +0800

    Add support for milvus 2.x (#510)
    Signed-off-by: dragondriver <jiquan.long@zilliz.com>
    
    Co-authored-by: dragondriver <jiquan.long@zilliz.com>

--- milvus/__init__.py
+++ milvus/__init__.py
@@ -2,16 +2,17 @@
 
 from .client.stub import Milvus
 from .client.prepare import Prepare
-from .client.types import IndexType, MetricType, Status
+from .client.types import Status, DataType, RangeType, IndexType, MetricType
 from .client.exceptions import (
     ParamError,
     ConnectError,
     NotConnectError,
     RepeatingConnectError,
-    VersionError
+    VersionError,
+    BaseException
 )
 from .client import __version__
 
-__all__ = ['Milvus', 'Prepare', 'Status', 'IndexType', 'MetricType',
-           'ParamError', 'ConnectError', 'NotConnectError', 'RepeatingConnectError', 'VersionError',
+__all__ = ['Milvus', 'Prepare', 'Status', 'DataType',
+           'ParamError', 'ConnectError', 'NotConnectError', 'RepeatingConnectError', 'VersionError', 'BaseException',
            '__version__']
