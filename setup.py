c919f6d4b21acc128790b153c15730ddd2dd85fb
XuanYang-cn <xuan.yang@zilliz.com>
2021-05-25 20:17:25 +0800

    Control required version for grpcio and grpcio-tools (#522)
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,12 @@ setuptools.setup(
     license="Apache-2.0",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=["grpcio>=1.22.0", "grpcio-tools>=1.22.0", "requests>=2.22.0", "ujson>=2.0.0"],
+    install_requires=[
+        "grpcio>=1.22.0,<1.38.0",
+        "grpcio-tools>=1.22.0, <1.38.0",
+        "requests>=2.22.0",
+        "ujson>=2.0.0",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
