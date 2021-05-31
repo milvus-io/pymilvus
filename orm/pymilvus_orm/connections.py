d3cc5c26fc8d63c085d4a0d60f196be290ffcd7d
cai.zhang <cai.zhang@zilliz.com>
2021-05-31 10:41:03 +0000

    Implement connections class by single instance
    * Implement connections class by single instance
    
    Signed-off-by: xiaocai2333 <cai.zhang@zilliz.com>
    
    * Fromat code
    
    Signed-off-by: xiaocai2333 <cai.zhang@zilliz.com>

--- orm/pymilvus_orm/connections.py
+++ orm/pymilvus_orm/connections.py
@@ -16,7 +16,21 @@ from .default_config import DefaultConfig
 from .exceptions import ParamError
 
 
-class Connections:
+class SingleInstanceMetaClass(type):
+    def __init__(cls, name, bases, dic):
+        cls.__single_instance = None
+        super().__init__(name, bases, dic)
+
+    def __call__(cls, *args, **kwargs):
+        if cls.__single_instance:
+            return cls.__single_instance
+        single_obj = cls.__new__(cls)
+        single_obj.__init__(*args, **kwargs)
+        cls.__single_instance = single_obj
+        return single_obj
+
+
+class Connections(metaclass=SingleInstanceMetaClass):
     """
     Connections is a class which is used to manage all connections of milvus.
     Used as a singleton in this module.
