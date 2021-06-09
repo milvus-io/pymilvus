6097dc3cb8458890657b9e86516f6902678cb8f0
Xiangyu Wang <xiangyu.wang@zilliz.com>
2021-06-09 11:04:34 +0800

    Add length method for field_data (#558)
    Signed-off-by: Xiangyu Wang <xiangyu.wang@zilliz.com>

--- examples/query.py
+++ examples/query.py
@@ -1,5 +1,6 @@
 from pymilvus import Milvus, DataType
 import random
+from pprint import pprint
 
 if __name__ == "__main__":
     c = Milvus("localhost", "19530")
@@ -15,6 +16,10 @@ if __name__ == "__main__":
             "indexes": [{"metric_type": "L2"}]
         },
         {
+            "name": "age",
+            "type": DataType.FLOAT,
+        },
+        {
             "name": "id",
             "type": DataType.INT64,
             "is_primary": True,
@@ -26,12 +31,13 @@ if __name__ == "__main__":
     assert c.has_collection(collection_name)
 
     c.insert(collection_name, [
-        {"name": "f1", "type": DataType.FLOAT_VECTOR, "values": [[1, 2, 3, 4]]},
-        {"name": "id", "type": DataType.INT64, "values": [1]}
+        {"name": "f1", "type": DataType.FLOAT_VECTOR, "values": [[1.1, 2.2, 3.3, 4.4], [5.5, 6.6, 7.7, 8.8]]},
+        {"name": "age", "type": DataType.FLOAT, "values": [3.45, 8.9]},
+        {"name": "id", "type": DataType.INT64, "values": [1, 2]}
     ], orm=True)
 
     c.flush([collection_name])
 
     c.load_collection(collection_name)
 
-    print(c.query(collection_name, f"id in [ 1 ]"))
+    pprint(c.query(collection_name, f"id in [ 1, 2 ]"))
