08b798be7bb2bee0c1c22569a99cd093aa08c62b
kun yu <kun.yu@zilliz.com>
2021-06-26 12:05:01 +0800

    Check if output_fields is list
    Signed-off-by: kun yu <kun.yu@zilliz.com>

--- pymilvus/client/grpc_handler.py
+++ pymilvus/client/grpc_handler.py
@@ -1006,6 +1006,8 @@ class GrpcHandler(AbsMilvus):
 
     @error_handler()
     def query(self, collection_name, expr, output_fields=None, partition_names=None, timeout=None):
+        if type(output_fields) == list:
+            raise BaseException(0, "Param output_fields should be a list")
         request = Prepare.query_request(collection_name, expr, output_fields, partition_names)
         future = self._stub.Query.future(request, wait_for_ready=True, timeout=timeout)
         response = future.result()
