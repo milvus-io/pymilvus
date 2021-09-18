ab58b1e292d93612437fc243d0becc64ee6ce56a
XuanYang-cn <xuan.yang@zilliz.com>
2021-09-18 16:27:22 +0800

    Remove extra pymilvus_orm directory
    Move tests out
    
    Signed-off-by: XuanYang-cn <xuan.yang@zilliz.com>

--- pymilvus/__init__.py
+++ pymilvus/__init__.py
@@ -27,12 +27,12 @@ from .client import __version__
 
 
 """client module"""
-from .orm.pymilvus_orm.collection import Collection
-from .orm.pymilvus_orm.connections import connections, Connections
+from .orm.collection import Collection
+from .orm.connections import connections, Connections
 
-from .orm.pymilvus_orm.index import Index
-from .orm.pymilvus_orm.partition import Partition
-from .orm.pymilvus_orm.utility import (
+from .orm.index import Index
+from .orm.partition import Partition
+from .orm.utility import (
     loading_progress,
     index_building_progress,
     wait_for_loading_complete,
@@ -43,13 +43,13 @@ from .orm.pymilvus_orm.utility import (
     drop_collection,
 )
 
-from .orm.pymilvus_orm import utility
-from .orm.pymilvus_orm.default_config import DefaultConfig
+from .orm import utility
+from .orm.default_config import DefaultConfig
 
-from .orm.pymilvus_orm.search import SearchResult, Hits, Hit
-from .orm.pymilvus_orm.schema import FieldSchema, CollectionSchema
-from .orm.pymilvus_orm.future import SearchFuture, MutationFuture
-from .orm.pymilvus_orm.exceptions import ExceptionsMessage
+from .orm.search import SearchResult, Hits, Hit
+from .orm.schema import FieldSchema, CollectionSchema
+from .orm.future import SearchFuture, MutationFuture
+from .orm.exceptions import ExceptionsMessage
 
 __all__ = [
     # pymilvus orm'styled APIs
