94434ad9faf4f3d0858c2267d7b42388095287b2
dragondriver <jiquan.long@zilliz.com>
2021-11-02 10:09:22 +0800

    Do not divide search requests
    Signed-off-by: dragondriver <jiquan.long@zilliz.com>

--- pymilvus/client/configs.py
+++ pymilvus/client/configs.py
@@ -1,4 +1,5 @@
 # TODO(dragondriver): add more default configs to here
 class DefaultConfigs:
-    MaxSearchResultSize = 100 * 1024 * 1024
+    # 40G / 10 = 4G, big enough
+    MaxSearchResultSize = 40 * 1024 * 1024 * 1024
     WaitTimeDurationWhenLoad = 0.5  # in seconds
