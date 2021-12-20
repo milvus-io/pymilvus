8d230ade63996e4af04f6eaf220a2d2ea97f40a0
XuanYang-cn <xuan.yang@zilliz.com>
2021-12-20 10:40:40 +0800

    Enable get git commit for a given version (#839)
    Signed-off-by: XuanYang-cn <xuan.yang@zilliz.com>

--- tests/test_check.py
+++ tests/test_check.py
@@ -4,6 +4,7 @@ import pytest
 from pymilvus.client.check import check_pass_param
 from pymilvus.client.utils import mkts_from_unixtime, mkts_from_datetime, mkts_from_hybridts, \
     hybridts_to_unixtime
+from pymilvus.client import get_commit
 
 
 class TestCheckPassParam:
@@ -57,3 +58,34 @@ class TestGenTS:
         t3 = mkts_from_datetime(dtime, delta=delta)
         timestamp3 = hybridts_to_unixtime(t3)
         assert timestamp1 == timestamp3
+
+
+class TestGetCommit:
+
+    def test_get_commit(self):
+        s = get_commit("2.0.0rc9.dev22")
+        assert s == "290d76f"
+
+        s = get_commit("2.0.0rc8", False)
+        assert s == "c9f015a04058638a28e1d2a5b265147cda0b0a23"
+
+    def test_version_re(self):
+        import re
+        version_info = r'((\d+)\.(\d+)\.(\d+))((rc)(\d+))?(\.dev(\d+))?'
+        p = re.compile(version_info)
+
+        versions = [
+            '2.0.0',
+            '2.0.0rc3',
+            '2.0.0rc4.dev8',
+            '2.0.0rc4.dev22',
+            '2.0.'
+        ]
+
+        for v in versions:
+            rv = p.match(v)
+            if rv is not None:
+                assert rv.group() == v
+
+                print(f"group {rv.group()}")
+                print(f"group {rv.groups()}")
