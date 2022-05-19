f40739bfccb316d297c12bb9d7f32da0e1add91c
zhenshan.cao <zhenshan.cao@zilliz.com>
2022-05-19 10:14:55 +0800

    Set guarantee_timestamp to 2 in case of Bounded consistency level
    Signed-off-by: zhenshan.cao <zhenshan.cao@zilliz.com>

--- pymilvus/client/constants.py
+++ pymilvus/client/constants.py
@@ -2,6 +2,6 @@ from ..grpc_gen.common_pb2 import ConsistencyLevel
 
 LOGICAL_BITS = 18
 LOGICAL_BITS_MASK = (1 << LOGICAL_BITS) - 1
-DEFAULT_GRACEFUL_TIME = 5000  # in ms
 EVENTUALLY_TS = 1
+BOUNDED_TS = 2
 DEFAULT_CONSISTENCY_LEVEL = ConsistencyLevel.Bounded
