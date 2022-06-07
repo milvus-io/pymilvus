2ff151ad2968b9ba19cdb9ed73688bb76e223b24
xige-16 <xi.ge@zilliz.com>
2022-06-07 13:07:47 +0800

    Alter varChar type params's name to max_length
    Signed-off-by: xige-16 <xi.ge@zilliz.com>

--- pymilvus/orm/constants.py
+++ pymilvus/orm/constants.py
@@ -10,7 +10,7 @@
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
 
-COMMON_TYPE_PARAMS = ("dim", "max_length_per_row")
+COMMON_TYPE_PARAMS = ("dim", "max_length")
 
 
 
