0e1aa63ff879412ac8e4f6765ab8bf9033c4244d
xige-16 <xi.ge@zilliz.com>
2022-06-07 13:14:02 +0800

    Alter varChar type params's name to max_length (#1024)
    Signed-off-by: xige-16 <xi.ge@zilliz.com>

--- pymilvus/client/configs.py
+++ pymilvus/client/configs.py
@@ -1,7 +1,7 @@
 # TODO(dragondriver): add more default configs to here
 class DefaultConfigs:
     WaitTimeDurationWhenLoad = 0.5  # in seconds
-    MaxVarCharLengthKey = "max_length_per_row"
+    MaxVarCharLengthKey = "max_length"
     MaxVarCharLength = 65535
     EncodeProtocol = 'utf-8'
     IndexName = "_default_idx"
