011d1e3015ff552125e950efda2bd3bf393befce
SimFG <bang.fu@zilliz.com>
2022-08-15 17:31:10 +0800

    Handle the `sed` and `grep` difference between Linux and Darwin
    Signed-off-by: SimFG <bang.fu@zilliz.com>

--- pymilvus/grpc_gen/python_gen.sh
+++ pymilvus/grpc_gen/python_gen.sh
@@ -7,6 +7,15 @@ python -m grpc_tools.protoc -I ${PROTO_DIR} --python_out=${OUTDIR}              
 python -m grpc_tools.protoc -I ${PROTO_DIR} --python_out=${OUTDIR}                              ${PROTO_DIR}/schema.proto
 python -m grpc_tools.protoc -I ${PROTO_DIR} --python_out=${OUTDIR} --grpc_python_out=${OUTDIR}  ${PROTO_DIR}/milvus.proto
 
+if [[ $(uname -s) == "Darwin" ]]; then
+    if ! brew --prefix --installed gnu-sed >/dev/null 2>&1; then
+        brew install gnu-sed
+    fi
+    export PATH="/usr/local/opt/gsed/libexec/gnubin:$PATH"
+fi
+
 sed -i 's/import common_pb2 as common__pb2/from . import common_pb2 as common__pb2/' $OUTDIR/*py
 sed -i 's/import schema_pb2 as schema__pb2/from . import schema_pb2 as schema__pb2/' $OUTDIR/*py
 sed -i 's/import milvus_pb2 as milvus__pb2/from . import milvus_pb2 as milvus__pb2/' $OUTDIR/*py
+
+echo "Success to generate the python proto files."
