5243efa07f46cfacd8cfede5928611b08b607e0c
longjiquan <jiquan.long@zilliz.com>
2023-05-08 19:20:04 +0800

    Remove unnecessary index check
    Signed-off-by: longjiquan <jiquan.long@zilliz.com>

--- pymilvus/client/grpc_handler.py
+++ pymilvus/client/grpc_handler.py
@@ -17,7 +17,6 @@ from .check import (
     is_legal_host,
     is_legal_port,
     check_pass_param,
-    check_index_params,
 )
 from .prepare import Prepare
 from .types import (
@@ -572,8 +571,6 @@ class GrpcHandler:
             valid_field = True
             if fields["type"] != DataType.FLOAT_VECTOR and fields["type"] != DataType.BINARY_VECTOR:
                 break
-            # check index params on vector field.
-            check_index_params(params)
 
         if not valid_field:
             raise MilvusException(message=f"cannot create index on non-existed field: {field_name}")
