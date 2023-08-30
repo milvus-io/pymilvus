ef04f43338ce153b2eb94bac668ccf19894efe7a
yhmo <yihua.mo@zilliz.com>
2023-08-30 19:10:45 +0800

    Bulkinsert writer
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

---
+++ pymilvus/bulk_writer/remote_bulk_writer.py
@@ -0,0 +1,155 @@
+# Copyright (C) 2019-2023 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+
+import logging
+from pathlib import Path
+from typing import Any, Optional
+
+from minio import Minio
+from minio.error import S3Error
+
+from pymilvus.orm.schema import CollectionSchema
+
+from .constants import (
+    DEFAULT_BUCKET_NAME,
+    MB,
+)
+from .local_bulk_writer import LocalBulkWriter
+
+logger = logging.getLogger("remote_bulk_writer")
+logger.setLevel(logging.DEBUG)
+
+
+class RemoteBulkWriter(LocalBulkWriter):
+    class ConnectParam:
+        def __init__(
+            self,
+            bucket_name: str = DEFAULT_BUCKET_NAME,
+            endpoint: Optional[str] = None,
+            access_key: Optional[str] = None,
+            secret_key: Optional[str] = None,
+            secure: bool = False,
+            session_token: Optional[str] = None,
+            region: Optional[str] = None,
+            http_client: Any = None,
+            credentials: Any = None,
+        ):
+            self._bucket_name = bucket_name
+            self._endpoint = endpoint
+            self._access_key = access_key
+            self._secret_key = secret_key
+            self._secure = (secure,)
+            self._session_token = (session_token,)
+            self._region = (region,)
+            self._http_client = (http_client,)  # urllib3.poolmanager.PoolManager
+            self._credentials = (credentials,)  # minio.credentials.Provider
+
+    def __init__(
+        self,
+        schema: CollectionSchema,
+        remote_path: str,
+        local_path: str,
+        connect_param: ConnectParam,
+        segment_size: int = 512 * MB,
+    ):
+        super().__init__(schema, local_path, segment_size)
+        uid = Path(super().data_path).name
+        self._remote_path = Path("/").joinpath(remote_path).joinpath(uid)
+        self._connect_param = connect_param
+        self._client = None
+        self._get_client()
+        logger.info(f"Remote buffer writer initialized, target path: {self._remote_path}")
+
+    def _get_client(self):
+        try:
+            if self._client is None:
+
+                def arg_parse(arg: Any):
+                    return arg[0] if isinstance(arg, tuple) else arg
+
+                self._client = Minio(
+                    endpoint=arg_parse(self._connect_param._endpoint),
+                    access_key=arg_parse(self._connect_param._access_key),
+                    secret_key=arg_parse(self._connect_param._secret_key),
+                    secure=arg_parse(self._connect_param._secure),
+                    session_token=arg_parse(self._connect_param._session_token),
+                    region=arg_parse(self._connect_param._region),
+                    http_client=arg_parse(self._connect_param._http_client),
+                    credentials=arg_parse(self._connect_param._credentials),
+                )
+            else:
+                return self._client
+        except Exception as err:
+            logger.error(f"Failed to connect MinIO/S3, error: {err}")
+            raise
+
+    def append_row(self, row: dict, **kwargs):
+        super().append_row(row, **kwargs)
+
+    def commit(self, **kwargs):
+        super().commit(call_back=self._upload)
+
+    def _remote_exists(self, file: str) -> bool:
+        try:
+            minio_client = self._get_client()
+            minio_client.stat_object(bucket_name=self._connect_param._bucket_name, object_name=file)
+        except S3Error as err:
+            if err.code == "NoSuchKey":
+                return False
+            self._throw(f"Failed to stat MinIO/S3 object, error: {err}")
+        return True
+
+    def _local_rm(self, file: str):
+        try:
+            Path.unlink(file)
+        except Exception:
+            logger.warning(f"Failed to delete local file: {file}")
+
+    def _upload(self, file_list: list):
+        remote_files = []
+        try:
+            logger.info("Prepare to upload files")
+            minio_client = self._get_client()
+            found = minio_client.bucket_exists(self._connect_param._bucket_name)
+            if not found:
+                self._throw(f"MinIO bucket '{self._connect_param._bucket_name}' doesn't exist")
+
+            for file_path in file_list:
+                ext = Path(file_path).suffix
+                if ext not in {".json", ".npy"}:
+                    continue
+
+                relative_file_path = str(file_path).replace(str(super().data_path), "")
+                minio_file_path = str(
+                    Path.joinpath(self._remote_path, relative_file_path.lstrip("/"))
+                )
+
+                if not self._remote_exists(minio_file_path):
+                    minio_client.fput_object(
+                        bucket_name=self._connect_param._bucket_name,
+                        object_name=minio_file_path,
+                        file_path=file_path,
+                    )
+                    logger.info(f"Upload file '{file_path}' to '{minio_file_path}'")
+                else:
+                    logger.info(f"Remote file '{minio_file_path}' already exists")
+                remote_files.append(minio_file_path)
+                self._local_rm(file_path)
+        except Exception as e:
+            self._throw(f"Failed to call MinIO/S3 api, error: {e}")
+
+        logger.info(f"Successfully upload files: {file_list}")
+        return remote_files
+
+    @property
+    def data_path(self):
+        return self._remote_path
