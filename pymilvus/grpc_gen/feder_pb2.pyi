5c2385f9b6ad041ec170cdcb42d01be48dbeb0be
XuanYang-cn <xuan.yang@zilliz.com>
2024-01-15 16:18:45 +0800

    [cherry-pick] Support python3.12 (#1861)
    See also: #1860
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- pymilvus/grpc_gen/feder_pb2.pyi
+++ pymilvus/grpc_gen/feder_pb2.pyi
@@ -7,7 +7,7 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class SegmentIndexData(_message.Message):
-    __slots__ = ["segmentID", "index_data"]
+    __slots__ = ("segmentID", "index_data")
     SEGMENTID_FIELD_NUMBER: _ClassVar[int]
     INDEX_DATA_FIELD_NUMBER: _ClassVar[int]
     segmentID: int
@@ -15,7 +15,7 @@ class SegmentIndexData(_message.Message):
     def __init__(self, segmentID: _Optional[int] = ..., index_data: _Optional[str] = ...) -> None: ...
 
 class FederSegmentSearchResult(_message.Message):
-    __slots__ = ["segmentID", "visit_info"]
+    __slots__ = ("segmentID", "visit_info")
     SEGMENTID_FIELD_NUMBER: _ClassVar[int]
     VISIT_INFO_FIELD_NUMBER: _ClassVar[int]
     segmentID: int
@@ -23,7 +23,7 @@ class FederSegmentSearchResult(_message.Message):
     def __init__(self, segmentID: _Optional[int] = ..., visit_info: _Optional[str] = ...) -> None: ...
 
 class ListIndexedSegmentRequest(_message.Message):
-    __slots__ = ["base", "collection_name", "index_name"]
+    __slots__ = ("base", "collection_name", "index_name")
     BASE_FIELD_NUMBER: _ClassVar[int]
     COLLECTION_NAME_FIELD_NUMBER: _ClassVar[int]
     INDEX_NAME_FIELD_NUMBER: _ClassVar[int]
@@ -33,7 +33,7 @@ class ListIndexedSegmentRequest(_message.Message):
     def __init__(self, base: _Optional[_Union[_common_pb2.MsgBase, _Mapping]] = ..., collection_name: _Optional[str] = ..., index_name: _Optional[str] = ...) -> None: ...
 
 class ListIndexedSegmentResponse(_message.Message):
-    __slots__ = ["status", "segmentIDs"]
+    __slots__ = ("status", "segmentIDs")
     STATUS_FIELD_NUMBER: _ClassVar[int]
     SEGMENTIDS_FIELD_NUMBER: _ClassVar[int]
     status: _common_pb2.Status
@@ -41,7 +41,7 @@ class ListIndexedSegmentResponse(_message.Message):
     def __init__(self, status: _Optional[_Union[_common_pb2.Status, _Mapping]] = ..., segmentIDs: _Optional[_Iterable[int]] = ...) -> None: ...
 
 class DescribeSegmentIndexDataRequest(_message.Message):
-    __slots__ = ["base", "collection_name", "index_name", "segmentsIDs"]
+    __slots__ = ("base", "collection_name", "index_name", "segmentsIDs")
     BASE_FIELD_NUMBER: _ClassVar[int]
     COLLECTION_NAME_FIELD_NUMBER: _ClassVar[int]
     INDEX_NAME_FIELD_NUMBER: _ClassVar[int]
@@ -53,9 +53,9 @@ class DescribeSegmentIndexDataRequest(_message.Message):
     def __init__(self, base: _Optional[_Union[_common_pb2.MsgBase, _Mapping]] = ..., collection_name: _Optional[str] = ..., index_name: _Optional[str] = ..., segmentsIDs: _Optional[_Iterable[int]] = ...) -> None: ...
 
 class DescribeSegmentIndexDataResponse(_message.Message):
-    __slots__ = ["status", "index_data", "index_params"]
+    __slots__ = ("status", "index_data", "index_params")
     class IndexDataEntry(_message.Message):
-        __slots__ = ["key", "value"]
+        __slots__ = ("key", "value")
         KEY_FIELD_NUMBER: _ClassVar[int]
         VALUE_FIELD_NUMBER: _ClassVar[int]
         key: int
