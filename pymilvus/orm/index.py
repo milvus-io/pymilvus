9d76a05da1743229b0cfc9b9fbeae7c56929cfd3
yangxuan <xuan.yang@zilliz.com>
2024-06-18 15:32:14 +0800

    enhance: update milvus-proto and correct index.drop()
    Index name and field name is already there for Index object,
    No need to get them from kwargs when drop
    
    ```python
    ivf_flat = Index(c, "embeddings", params, index_name="ivf_flat",
        construct_only=True)
    ivf_flat.drop()
    ```
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- pymilvus/orm/index.py
+++ pymilvus/orm/index.py
@@ -73,10 +73,8 @@ class Index:
         self._collection = collection
         self._field_name = field_name
         self._index_params = index_params
-        index_name = kwargs.get("index_name", Config.IndexName)
-        self._index_name = index_name
-        self._kwargs = kwargs
-        if self._kwargs.pop("construct_only", False):
+        self._index_name = kwargs.get("index_name", Config.IndexName)
+        if kwargs.get("construct_only", False):
             return
 
         conn = self._get_connection()
@@ -137,5 +135,4 @@ class Index:
             field_name=self.field_name,
             index_name=self.index_name,
             timeout=timeout,
-            **kwargs,
         )
