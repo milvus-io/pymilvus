07052c5e5fad9f41a681e693646cbb3e2f7a54f1
Patrick Weizhi Xu <weizhi.xu@zilliz.com>
2024-09-03 14:10:57 +0800

    fix: [2.4] move page_retain_order to the same level as radius (#2250)
    issue: https://github.com/milvus-io/milvus/issues/35464
    
    Signed-off-by: Patrick Weizhi Xu <weizhi.xu@zilliz.com>
    (cherry picked from commit 358d0dc5bd4ed2bbfec1dd1eb28d98ed246a4dfe)

--- pymilvus/orm/collection.py
+++ pymilvus/orm/collection.py
@@ -681,8 +681,6 @@ class Collection:
                     similar metricy types, the value must be of type str.
                 * *offset* (``int``, optional)
                     offset for pagination.
-                * *page_retain_order* (``bool``, optional)
-                    Whether to retain the order of the search result when offset is provided.
                 * *params of index: *nprobe*, *ef*, *search_k*, etc
                     Corresponding search params for a certain index.
                 example for param::
@@ -690,7 +688,6 @@ class Collection:
                     {
                         "metric_type": "L2",
                         "offset": 10,
-                        "page_retain_order": True,
                         "params": {"nprobe": 12},
                     }
 
@@ -722,9 +719,6 @@ class Collection:
 
                 * *offset* (``int``, optinal)
                     offset for pagination.
-
-                * *page_retain_order* (``bool``, optional)
-                    Whether to retain the order of the search result when offset is provided.
 
                 * *consistency_level* (``str/int``, optional)
                     Which consistency level to use when searching in the collection.
