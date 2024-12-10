0cbfc7a8e6204e724c9b371c9c3c91aeee8d259e
Xianhui.Lin <xianhui.lin@zilliz.com>
2024-12-10 15:35:07 +0800

    retain interface set_properties&&alterindex
    Signed-off-by: Xianhui.Lin <xianhui.lin@zilliz.com>

--- pymilvus/orm/collection.py
+++ pymilvus/orm/collection.py
@@ -341,6 +341,34 @@ class Collection:
         conn = self._get_connection()
         conn.drop_collection(self._name, timeout=timeout, **kwargs)
 
+    def set_properties(self, properties: dict, timeout: Optional[float] = None, **kwargs):
+        """Set properties for the collection
+        Args:
+            properties (``dict``): collection properties.
+                 support collection TTL with key `collection.ttl.seconds`
+                 support collection replica number with key `collection.replica.number`
+                 support collection resource groups with key `collection.resource_groups`.
+            timeout (float, optional): an optional duration of time in seconds to allow
+                for the RPCs. If timeout is not set, the client keeps waiting until the
+                server responds or an error occurs.
+        Examples:
+            >>> from pymilvus import Collection, FieldSchema, CollectionSchema, DataType
+            >>> fields = [
+            ...     FieldSchema("film_id", DataType.INT64, is_primary=True),
+            ...     FieldSchema("films", dtype=DataType.FLOAT_VECTOR, dim=128)
+            ... ]
+            >>> schema = CollectionSchema(fields=fields)
+            >>> collection = Collection("test_set_properties", schema)
+            >>> collection.set_properties({"collection.ttl.seconds": 60})
+        """
+        conn = self._get_connection()
+        conn.alter_collection_properties(
+            self.name,
+            properties=properties,
+            timeout=timeout,
+            **kwargs,
+        )
+        
     def alter_collection_properties(self, properties: dict, timeout: Optional[float] = None, **kwargs):
         """Set properties for the collection
 
@@ -1390,6 +1418,44 @@ class Collection:
         """
         conn = self._get_connection()
         return conn.create_index(self._name, field_name, index_params, timeout=timeout, **kwargs)
+
+    def alter_index(
+        self,
+        index_name: str,
+        extra_params: dict,
+        timeout: Optional[float] = None,
+    ):
+        """Alter index for a specified field, with a index name.
+        Args:
+            index_name (``str``): The name of the index to alter
+            extra_params (``dict``): The parameters to index
+                * *mmap.enabled* (``str``)
+                    "mmap.enabled" as the key, example values: True or False.
+            timeout (``float``, optional): An optional duration of time in seconds to allow
+                for the RPC. When timeout is set to None, client waits until server
+                response or error occur.
+        Raises:
+            MilvusException: If anything goes wrong.
+        Examples:
+            >>> from pymilvus import Collection, FieldSchema, CollectionSchema, DataType
+            >>> from pymilvus import IndexType, MetricType
+            >>> schema = CollectionSchema([
+            ...     FieldSchema("film_id", DataType.INT64, is_primary=True),
+            ...     FieldSchema("title", DataType.STRING),
+            ...     FieldSchema("films", dtype=DataType.FLOAT_VECTOR, dim=2)
+            ... ])
+            >>> collection = Collection("test_collection_alter_index", schema)
+            >>> index_params = {
+            ...     "index_type": IndexType.IVF_FLAT,
+            ...     "metric_type": MetricType.L2,
+            ...     "params": {"nlist": 128}
+            ... }
+            >>> collection.create_index("films", index_params, index_name="idx")
+            Status(code=0, message='')
+            >>> collection.alter_index("idx", {"mmap.enabled": True})
+        """
+        conn = self._get_connection()
+        return conn.alter_index_properties(self._name, index_name, extra_params, timeout=timeout)
 
     def alter_index_properties(
         self,
