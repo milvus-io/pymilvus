4cdc441c2d799b23c64d1b6090218da509326186
shaoting-huang <shaoting.huang@zilliz.com>
2024-12-11 14:30:49 +0800

    rbac privilege group and grant v2 examples
    Signed-off-by: shaoting-huang <shaoting.huang@zilliz.com>

--- examples/rbac.py
+++ examples/rbac.py
@@ -53,6 +53,10 @@ db_ro_privileges = [
 
 role_db_rw = "db_rw"
 role_db_ro = "db_ro"
+role_custom = "custom_role"
+role_cluster_admin = "cluster_admin"
+role_database_readonly = "database_readonly"
+role_collection_readwrite = "collection_readwrite"
 
 current_roles = milvus_client.list_roles()
 print("current roles:", current_roles)
@@ -64,7 +68,31 @@ for role in [role_db_rw, role_db_ro]:
             milvus_client.revoke_privilege(role, item["object_type"], item["privilege"], item["object_name"])
         
         milvus_client.drop_role(role)
-        
+
+# manage custom privilege group and grant it to custom role
+privilege_group_name = "custom_privilege_group"
+milvus_client.create_privilege_group(privilege_group_name)
+milvus_client.add_privileges_to_group(privilege_group_name, ["Search", "Query"])
+milvus_client.list_privilege_groups()
+milvus_client.remove_privileges_from_group(privilege_group_name, ["Search"])
+milvus_client.list_privilege_groups()
+milvus_client.create_role(role_custom)
+milvus_client.grant_privilege_v2(role_custom, privilege_group_name, "*")
+
+# grant cluster level built-in privilege group
+milvus_client.create_role(role_cluster_admin)
+milvus_client.grant_privilege_v2(role_cluster_admin, "ClusterAdmin", "*", "*")
+
+# grant database level built-in privilege group
+milvus_client.create_role(role_database_readonly)
+milvus_client.grant_privilege_v2(role_database_readonly, "DatabaseReadOnly", "*", "db1")
+
+# grant collection level built-in privilege group
+milvus_client.create_role(role_collection_readwrite)
+milvus_client.grant_privilege_v2(role_collection_readwrite, "CollectionReadWrite", "col1", "db1")
+
+roles = milvus_client.list_roles()
+print("roles:", roles)
 
 milvus_client.create_role(role_db_rw)
 for item in db_rw_privileges:
@@ -88,9 +116,12 @@ print("user info for user1:", user1_info)
 print(f"grant {role_db_rw} to user1")
 milvus_client.grant_role("user1", role_db_rw)
 print("user info for user1:", user1_info)
+milvus_client.grant_role("user1", role_collection_readwrite)
 
 milvus_client.grant_role("user2", role_db_ro)
 milvus_client.grant_role("user2", role_db_rw)
+milvus_client.grant_role("user2", role_database_readonly)
+milvus_client.grant_role("user2", role_cluster_admin)
 
 user2_info = milvus_client.describe_user("user2")
 print("user info for user2:", user2_info)
@@ -101,4 +132,23 @@ print("user info for user2:", user2_info)
 
 user3_info = milvus_client.describe_user("user3")
 print("user info for user3:", user3_info)
+
+
+# revoke all privileges before dropping roles and users
+milvus_client.revoke_privilege_v2(role_cluster_admin, "ClusterAdmin", "*", "*")
+milvus_client.revoke_privilege_v2(role_database_readonly, "DatabaseReadOnly", "*", "db1")
+milvus_client.revoke_privilege_v2(role_collection_readwrite, "CollectionReadWrite", "col1", "db1")
+milvus_client.revoke_privilege_v2(role_custom, privilege_group_name, "*")
+
+milvus_client.drop_role(role_cluster_admin)
+milvus_client.drop_role(role_database_readonly)
+milvus_client.drop_role(role_collection_readwrite)
+milvus_client.drop_role(role_custom)
+milvus_client.list_roles
+
+milvus_client.drop_privilege_group(privilege_group_name)
+
+milvus_client.drop_user("user1")
+milvus_client.drop_user("user2")
+milvus_client.drop_user("user3")    
 
