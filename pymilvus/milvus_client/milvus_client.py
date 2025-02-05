3e133febc2eac26b3de11180673f236a9197c4c7
Xianhui.Lin <xianhui.lin@zilliz.com>
2025-02-05 20:21:41 +0800

    Revert "feat:get and delete use schema cache"
    This reverts commit 8959012ff921b4e18e9c51a021143af5df8d6ff6.

--- pymilvus/milvus_client/milvus_client.py
+++ pymilvus/milvus_client/milvus_client.py
@@ -710,7 +710,7 @@ class MilvusClient:
 
         conn = self._get_connection()
         try:
-            schema_dict, _ = conn._get_schema_from_cache_or_remote(collection_name, timeout=timeout)
+            schema_dict = conn.describe_collection(collection_name, timeout=timeout, **kwargs)
         except Exception as ex:
             logger.error("Failed to describe collection: %s", collection_name)
             raise ex from ex
@@ -796,7 +796,7 @@ class MilvusClient:
         conn = self._get_connection()
         if len(pks) > 0:
             try:
-                schema_dict , _ = conn._get_schema_from_cache_or_remote(collection_name, timeout=timeout)
+                schema_dict = conn.describe_collection(collection_name, timeout=timeout, **kwargs)
             except Exception as ex:
                 logger.error("Failed to describe collection: %s", collection_name)
                 raise ex from ex
