66ad7ccd3dd983180335d8eaea799e787b06b110
yangxuan <xuan.yang@zilliz.com>
2025-04-23 12:24:57 +0800

    fix: Omit empty index types
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- pymilvus/milvus_client/index.py
+++ pymilvus/milvus_client/index.py
@@ -50,10 +50,11 @@ class IndexParam:
                 "efConstruction": 100,
             }
         """
-        return {
-            "index_type": self.index_type,
-            **self._configs,
-        }
+        configs =  self._configs
+        # Omit index_type if it's empty
+        if self.index_type:
+            configs["index_type"] = self.index_type
+        return configs
 
     def to_dict(self) -> Dict:
         """All params"""
