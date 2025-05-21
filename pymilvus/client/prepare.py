4ee831f24f2a101a361a0a4a8c6e1e61851bf4e2
XuanYang-cn <xuan.yang@zilliz.com>
2025-05-21 17:52:19 +0800

    fix: load collection with resource group (#2800)
    See also: #2798
    
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- pymilvus/client/prepare.py
+++ pymilvus/client/prepare.py
@@ -1263,7 +1263,7 @@ class Prepare:
 
         if "resource_groups" in kwargs or "_resource_groups" in kwargs:
             resource_groups = kwargs.get("resource_groups", kwargs.get("_resource_groups"))
-            req.resource_groups = resource_groups
+            req.resource_groups.extend(resource_groups)
 
         if "load_fields" in kwargs or "_load_fields" in kwargs:
             load_fields = kwargs.get("load_fields", kwargs.get("_load_fields"))
@@ -1311,7 +1311,7 @@ class Prepare:
 
         if "resource_groups" in kwargs or "_resource_groups" in kwargs:
             resource_groups = kwargs.get("resource_groups", kwargs.get("_resource_groups"))
-            req.resource_groups = resource_groups
+            req.resource_groups.extend(resource_groups)
 
         if "load_fields" in kwargs or "_load_fields" in kwargs:
             load_fields = kwargs.get("load_fields", kwargs.get("_load_fields"))
