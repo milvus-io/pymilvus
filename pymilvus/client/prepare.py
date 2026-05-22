d9efd78d319fc60afb0a42fd80c332f6fbfc2f4f
bigsheeper <yihao.dai@zilliz.com>
2026-05-22 12:23:47 +0800

    feat: add Prepare.get_replicate_info_request
    Build a GetReplicateInfoRequest from source_cluster_id and
    target_pchannel. Raise ParamError when either is empty/None, matching
    the validation style used by update_replicate_configuration_request.
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- pymilvus/client/prepare.py
+++ pymilvus/client/prepare.py
@@ -2734,6 +2734,23 @@ class Prepare:
             force_promote=force_promote,
         )
 
+    @classmethod
+    def get_replicate_info_request(
+        cls,
+        source_cluster_id: Optional[str] = None,
+        target_pchannel: Optional[str] = None,
+    ):
+        if not source_cluster_id:
+            msg = "'source_cluster_id' must be provided"
+            raise ParamError(message=msg)
+        if not target_pchannel:
+            msg = "'target_pchannel' must be provided"
+            raise ParamError(message=msg)
+        return milvus_types.GetReplicateInfoRequest(
+            source_cluster_id=source_cluster_id,
+            target_pchannel=target_pchannel,
+        )
+
     @staticmethod
     def convert_function_to_function_schema(f: Function) -> schema_types.FunctionSchema:
         function_schema = schema_types.FunctionSchema(
